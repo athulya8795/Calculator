380fad80a7467947a6137429554760073a3cca91
athulya8795 <athulyamalu68@gmail.com>
2024-08-29 08:31:39 +0530

    hosted

---
+++ calculator.js
@@ -0,0 +1,20 @@
+//function display
+function display(num){
+    result.value += num
+}
+function clearAll(){
+    result.value=""
+}
+function Back(){
+    result.value=result.value.slice(0,-1)
+}
+function equal(){
+    try{
+        result.value=eval(result.value)
+    }catch(error){
+        result.value='Error'
+        setTimeout(()=>{
+            result.value=""
+        },1000)
+    }
+}
